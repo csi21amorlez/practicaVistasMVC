4ad8977a9eef1275b03ab962f89f4cd514f74d0a
Amglezdev <angelmoragonzalez216@outlook.com>
2022-11-14 19:30:07 +0100

    Commit inicial

---
+++ Controllers/UsuariosController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ejemploMVC_vistas.Controllers
+{
+    public class UsuariosController : Controller
+    {
+        // GET: Usuarios
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ListaUsuarios()
+        {
+            List<string> listUsuarios = new List<string>();
+            listUsuarios.Add("Fernando");
+            listUsuarios.Add("Gustavo");
+            listUsuarios.Add("Carlos");
+            listUsuarios.Add("Alfredo");
+            listUsuarios.Add("Felipe");
+            listUsuarios.Add("Roman");
+            listUsuarios.Add("Ricardo");
+
+            return View(listUsuarios);
+
+        }
+    }
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ejemploMVC_vistas.MvcApplication" Language="C#" %>
